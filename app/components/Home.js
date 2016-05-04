a54e99f46642d1af0bc484335fe0cbdbe7826c8c
Jaz Lalli <jazlalli@hotmail.com>
2016-05-04 09:56:24 +0100

    Add eslint to build process and fix first errors

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,14 +1,8 @@
 import React from 'react';
 
-const Home = React.createClass({
-  render: () => {
-    return (
-      <div>
-        <h1>Something something dark side!</h1>
-        <p>Something something complete!</p>
-      </div>
-    );
-  }
-});
-
-export default Home;
+export default () => (
+  <div>
+    <h1>Something something dark side!!!</h1>
+    <p>Something something complete!!!</p>
+  </div>
+);
--- bin/develop.sh
+++ bin/develop.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-mkdir -p dist/ && watchify app/main.js -t [ babelify --presets [ es2015 react stage-0 ] ] -o dist/bundle.js -dv &
-nodemon --ignore './app' --exec babel-node ./server.js
+mkdir -p dist/
+esw app/** & watchify app/main.js -t [ babelify --presets [ es2015 react stage-0 ] ] -o dist/bundle.js -dv & nodemon --ignore 'app/' --ignore 'dist/' --exec babel-node ./server.js
